c2e8aa9577ee41b371181cefad3a2385282ee3a4
cungquang <cungquang1987@gmail.com>
2024-03-11 10:58:36 -0700

    update hardware level code

---
+++ audio_cmake/hal/include/hal_helper.h
@@ -0,0 +1,9 @@
+#ifndef _HAL_HELPER_H_
+#define _HAL_HELPER_H_
+
+#include <stdio.h>
+#include <time.h>
+
+void runCommand(char* command);
+
+#endif
---
+++ audio_cmake/hal/src/hal_helper.c
@@ -0,0 +1,82 @@
+#include "../include/hal_helper.h"
+
+
+void runCommand(char* command)
+{
+    // Execute the shell command (output into pipe)
+    FILE *pipe = popen(command, "r");
+
+    // Ignore output of the command; but consume it
+    // so we don't get an error when closing the pipe.
+    char buffer[1024];
+    while (!feof(pipe) && !ferror(pipe)) {
+        if (fgets(buffer, sizeof(buffer), pipe) == NULL)
+            break;
+            // printf("--> %s", buffer); // Uncomment for debugging
+    }
+    // Get the exit code from the pipe; non-zero is an error:
+    int exitCode = WEXITSTATUS(pclose(pipe));
+    if (exitCode != 0) {
+        perror("Unable to execute command:");
+        printf(" command: %s\n", command);
+        printf(" exit code: %d\n", exitCode);
+    }
+}
+
+//Sleep for miliseconds
+static void sleepForMs(long long delayInMs)
+{
+    const long long NS_PER_MS = 1000 * 1000;
+    const long long NS_PER_SECOND = 1000000000;
+    long long delayNs = delayInMs * NS_PER_MS;
+    int seconds = delayNs / NS_PER_SECOND;
+    int nanoseconds = delayNs % NS_PER_SECOND;
+    struct timespec reqDelay = {seconds, nanoseconds};
+    nanosleep(&reqDelay, (struct timespec *) NULL);
+}
+
+static long long getTimeInMs(void)
+{
+    struct timespec spec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+    long long seconds = spec.tv_sec;
+    long long nanoSeconds = spec.tv_nsec;
+    long long milliSeconds = seconds * 1000 + nanoSeconds / 1000000;
+    return milliSeconds;
+}
+
+//Read from file to screen
+void readFromFileToScreen(char *fileName)
+{
+    FILE *pFile = fopen(fileName, "r");
+    if (pFile == NULL) {
+        printf("ERROR: Unable to open file (%s) for read\n", fileName);
+        exit(-1);
+    }
+    // Read string (line)
+    const int MAX_LENGTH = 1024;
+    char buff[MAX_LENGTH];
+    fgets(buff, MAX_LENGTH, pFile);
+    
+    // Close
+    fclose(pFile);
+    printf("Read: '%s'\n", buff);
+}
+
+
+void writeToFile(char * fileName)
+{
+    // Use fopen() to open the file for write access.
+    //FILE *pFile = fopen("/sys/class/gpio/export", "w");
+    FILE *pFile = fopen(fileName, "w");
+    if (pFile == NULL) {
+        printf("ERROR: Unable to open export file.\n");
+        exit(1);
+    }
+    // Write to data to the file using fprintf():
+    fprintf(pFile, "%d", 30);
+
+    // Close the file using fclose():
+    fclose(pFile);
+    // Call nanosleep() to sleep for ~300ms before use.
+}
