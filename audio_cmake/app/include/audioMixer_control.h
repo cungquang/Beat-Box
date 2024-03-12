51b75d2fa62dc5f6454a057ae5cda4b73d9be54b
cungquang <cungquang1987@gmail.com>
2024-03-11 21:23:23 -0700

    add comment

--- audio_cmake/app/include/audioMixer_control.h
+++ audio_cmake/app/include/audioMixer_control.h
@@ -12,19 +12,19 @@ void AudioMixerControl_init(int *terminateFlag);
 void AudioMixerControl_join(void);
 void AudioMixerControl_cleanup(void);
 
-//Drum control - add drum to queue
-void AudioMixerControl_Adddrum(int drumIndex);
-
 //Beat control - Select or unselect beats
 void AudioMixerControl_modifyBeat(int beatIndex, int value);
 
-//Sound control
+//Volume control
 void AudioMixerControl_setVolume(int newVolume);
 int AudioMixerControl_getVolume();
 
 //Tempo control
 void AudioMixerControl_setTempo(int newTempo);
 int AudioMixerControl_getTempo();
+
+//Drum control - add drum to queue
+void AudioMixerControl_Adddrum(int drumIndex);
 
 
 #endif
