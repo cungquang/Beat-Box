0919b7991cac9911a4f7316c8a11720c5f02e691
cungquang <cungquang1987@gmail.com>
2024-03-13 02:07:55 -0700

    create new class

---
+++ audio_cmake/app/include/application_control.h
@@ -0,0 +1,12 @@
+#ifndef _APPLICATION_CONTROL_H
+#define _APPLICATION_CONTROL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+
+#endif
