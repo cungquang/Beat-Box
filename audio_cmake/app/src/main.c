7f06f1710e3e32bd281be5b6c642392a562a1d82
cungquang <cungquang1987@gmail.com>
2024-03-15 01:01:39 -0700

    fix bugs

--- audio_cmake/app/src/main.c
+++ audio_cmake/app/src/main.c
@@ -11,10 +11,12 @@ void Operation()
     AudioMixerControl_init();
     JoystickControl_init();
     ProcessTime_init();
+    I2cbus1Control_init();
 
-    AudioMixerControl_join();
-    JoystickControl_join();
+    I2cbus1Control_join();
     ProcessTime_join();
+    JoystickControl_join();
+    AudioMixerControl_join();
     UDP_join();
 
     //Cleanup -> later move to shutdown thread
@@ -22,42 +24,11 @@ void Operation()
     AudioMixerControl_cleanup();
     JoystickControl_cleanup();
     ProcessTime_cleanup();
-}
-
-void test()
-{
-    unsigned char test;
-    //readFromProcessUptime();
-    I2cbus1_init();
-    I2cbus1Write_Reg1(0x27);
-
-    test = I2cbus1Read_OutXH();
-    printf("OUT_X_H: %d ---> %hhu\n", I2c1FileDesc_get(), test);
-    test = I2cbus1Read_OutYH();
-    printf("OUT_Y_H: %d ---> %hhu\n", I2c1FileDesc_get(), test);
-    test = I2cbus1Read_OutZH();
-    printf("OUT_Z_H: %d ---> %hhu\n", I2c1FileDesc_get(), test);
-
-    printf("-------------------------------------\n");
-
-    test = I2cbus1Read_OutXL();
-    printf("OUT_X_L: %d ---> %hhu\n", I2c1FileDesc_get(), test);
-    test = I2cbus1Read_OutYL();
-    printf("OUT_Y_L: %d ---> %hhu\n", I2c1FileDesc_get(), test);
-    test = I2cbus1Read_OutZL();
-    printf("OUT_Z_L: %d ---> %hhu\n", I2c1FileDesc_get(), test);
-
+    I2cbus1Control_cleanup();
 }
 
 int main() 
 {
-    UDP_initServer();
-    I2cbus1Control_init();
-
-    I2cbus1Control_join();
-    UDP_join();
-
-    UDP_cleanup();
 
     return 0;
 }
